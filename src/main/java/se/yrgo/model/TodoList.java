a0b38bcc947a4d08d29df4bc8c22016d2c9038b3
andreljungsvik <andre.ljungsvik@gmail.com>
2024-12-17 11:53:39 +0100

    TodoList class

---
+++ src/main/java/se/yrgo/model/TodoList.java
@@ -0,0 +1,30 @@
+package se.yrgo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoList {
+    private List<Task> tasks;
+
+    public TodoList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void removeTask(Task task) {
+        tasks.remove(task);
+    }
+
+    public List<Task> filterTasks(boolean completed) {
+        List<Task> filtered = new ArrayList<>();
+        for (Task task : tasks) {
+            if (task.isCompleted() == completed) {
+                filtered.add(task);
+            }
+        }
+        return filtered;
+    }
+}

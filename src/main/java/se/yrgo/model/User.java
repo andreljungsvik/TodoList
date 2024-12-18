0b3f4b6877f144ab3dc45989b3e43fbe50cfb704
foamel <luohuanyu3124@gmail.com>
2024-12-18 13:21:48 +0100

    add option to view tasks and exception handling in User

--- src/main/java/se/yrgo/model/User.java
+++ src/main/java/se/yrgo/model/User.java
@@ -1,23 +1,20 @@
 package se.yrgo.model;
 
-import se.yrgo.model.*;
+import java.util.Scanner;
 
 public class User {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            while (true) {
-                System.out.println("Current tasks");
-                //display tasks
-
-                System.out.printf("1. Create new task%n2. Remove task%n3. Filter tasks");
-                String in = sc.nextLine;
+        while(true) {
+            try (Scanner sc = new Scanner(System.in)) {
+                System.out.printf("1. Create new task%n2. Remove task%n3. View tasks");
+                String in = sc.nextLine();
                 TodoList tdl = new TodoList();
                 switch (in) {
                     case "1":
                         System.out.print("Name the task: ");
-                        String name = sc.nextLine;
+                        String name = sc.nextLine();
                         System.out.print("Task description: ");
-                        String desc = sc.nextLine;
+                        String desc = sc.nextLine();
                         tdl.addTask(new Task(name, desc));
                         break;
                     case "2":
@@ -27,21 +24,24 @@ public class User {
                         break;
                     case "3":
                         System.out.printf("1. Show completed tasks%n2. Show incomplete tasks");
-                        in = sc.nextInt;
-                        if (in == 1) {
-                            tdl.filterTasks(true);
+                        in = sc.nextLine();
+                        if (in.equals("1")) {
+                            for (Task task : tdl.filterTasks(true)) {
+                                System.out.println(task);
+                            }
                         }
-                        else if (in == 2) {
-                            tdl.filterTasks(false);
+                        else if (in.equals("2")) {
+                            for (Task task : tdl.filterTasks(false)) {
+                                System.out.println(task);
+                            }
                         }
                         break;
                     default:
-        
-                        break;
+                        throw new IllegalArgumentException("Invalid input");
                 }
+            } catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
             }
-        } catch (Exception e) {
-            // TODO: handle exception
         }
     }
 }
